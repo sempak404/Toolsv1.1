e50633f3b957919422f7b140034ed79dae15acbe
sempak404 <54473839+sempak404@users.noreply.github.com>
2019-09-29 12:52:24 +0700

    Add files via upload

---
+++ admin-anggota.sh
@@ -0,0 +1,37 @@
+clear
+bi='\033[34;1m' #biru                                              
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple                                           
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah                                             
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+
+echo $me"╞════════════════╗"
+echo $me"╞" $pu"Leader"$me
+echo $me"╞════════════════╝"
+echo ""
+echo "╞═══════════════════════╗"
+echo $me"╞" $pu"Tn.Fotolia"$me
+echo "╞═══════════════════════╝"
+echo ""
+echo ""
+echo "===============Anggota================" | lolcat
+echo $me"╞═════════════════════════════════════╗"
+echo "╞"$me"MrSempak ~ L0C4l666gh06t"
+echo "╞"$me"MŘ.ρą$µяąţąɲ.999 /ŁΜβ007" 
+echo "╞"$me"Hz3999Ghost ~ ./Queen"
+echo "╞"$me"Mr.Yaz" 
+echo "╞"$me"The Zero Day Exp~999"
+echo "╞"$me"MR_B4C0T"
+echo "╞"$me"Z-1ND3X"
+echo $pu"╞═════════════════════════════════════╝"
