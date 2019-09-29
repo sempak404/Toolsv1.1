bedd4175195b195aa0e6d11f2b6c8cc7cd16f003
sempak404 <54473839+sempak404@users.noreply.github.com>
2019-09-29 12:51:35 +0700

    Add files via upload

---
+++ IAFv1.1.sh
@@ -0,0 +1,220 @@
+clear
+echo "=======================================================================================[]" | lolcat
+echo "=======================================================================================[]" | lolcat
+echo "╔══╗╔═╦╗╔══╗╔═╗╔═╦╗╔═╗╔══╗╔══╗╔══╗╔═╦╗"
+echo "╚║║╝║║║║╚╗╗║║║║║║║║║╦╝║══╣╚║║╝║╔╗║║║║║"
+echo "╔║║╗║║║║╔╩╝║║║║║║║║║╩╗╠══║╔║║╗║╠╣║║║║║"
+echo "╚══╝╚╩═╝╚══╝╚═╝╚╩═╝╚═╝╚══╝╚══╝╚╝╚╝╚╩═╝"
+echo "=======================================================================================[]" | lolcat
+echo "=======================================================================================[]" | lolcat
+echo
+echo "┼────────────────────────────────────IAF───────────────────────────────────┼" | lolcat
+echo "√ By               : Sandi:): & TnFotolia                            []" | lolcat
+echo "√ Team             : Indonesian Anon Fighter                         []" | lolcat
+echo "√ TanggalToolDibuat: 27-09-2019                                      []" | lolcat
+echo "┼────────────────────────────────────IAF───────────────────────────────────┼" | lolcat
+echo
+echo "</>[]>>>>>>>>>>"
+read -p "Nama Lu :" name
+echo ""
+read -p "Kelas Brp Lu :" name1
+echo ""
+clear
+echo "=======================================================================================[]" | lolcat            
+echo "=======================================================================================[]" | lolcat
+toilet -f slant --gay " IAF "                                                                        
+echo "=======================================================================================[]" | lolcat
+echo "=======================================================================================[]" | lolcat
+echo                                                                                                                 
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>ToolsSempak<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" | lolcat
+echo "√ By               : Sandi:): & TnFotolia                            []" | lolcat                              
+echo "√ Team             : Indonesian Anon Fighter                         []" | lolcat                              
+echo "√ TanggalToolDibuat: 27-09-2019                                      []" | lolcat
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>Go!!!!!!!!!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" | lolcat
+echo ""
+echo "╞══════════════════════════════════════════════"
+echo $me"<$pu Welcome $name To Indonesian Anon Fighter" | lolcat
+echo "╞══════════════════════════════════════════════"
+echo ""
+echo "╞══════════════════════════════════════════════"
+echo $me"<$pu Njirr kls $name1 Gila Njirr" | lolcat
+echo "╞══════════════════════════════════════════════"
+echo ""
+echo "╞═══════════════════════════════════════════════════" | lolcat
+echo "╞" $pu"∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆" | lolcat
+echo "╞" $pu"ToolsNya" | lolcat
+echo "╞" $pu"∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆" | lolcat
+echo "╞═══════════════════════════════════════════════════" | lolcat
+echo ""
+echo "  0.Di harapkan Install Bahan Bahannya Dulu" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 1.Spam Call" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 2.Ddos Attaacker" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 3.Dark Fbv1.6" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 4.Kreator Script Deface" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 5.Install Lazymux" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 6.Install hunner Frame work" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 7.SQL Map" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 8.Admin Finder" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 9.Lacak IP" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 10.Ingin Masuk Team IAF Admin1" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 11.Ingin Masuk Team IAF admin2" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 12.Saycheese Sadap Kamera" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 13.Boom Spam" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 14.Bermain Moon-buggy" | lolcat
+echo "╞═════════════════════════════════════════╕"
+echo "╞ 15.keluar"
+echo "╞═════════════════════════════════════════╕"
+read -p "╞═══════Pilih═══>" pil
+
+case $pil in
+
+0) bash bahan.sh
+
+;;
+
+1) git clone https://github.com/siputra12/prank
+cd prank
+ls
+php wa.php
+
+;;
+
+2) git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+
+;;
+
+3) apt upgrade && apt update
+pkg install git
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+
+;;
+
+4) apt update
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/Ubaii/script-deface-creator
+ls
+cd script-deface-creator
+ls
+chmod +x create.py
+ls
+python2 create.py
+
+;;
+
+5) pkg update && upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+chmod +x lazymux.py
+python2 lazymux.py
+
+;;
+
+6) apt update
+apt install python
+apt install git -y
+git clone https://github.com/b3-v3r/Hunner
+cd Hunner
+chmod 777 hunner.py
+python hunner.py
+
+;;
+
+7) apt update
+apt install python
+apt install python2
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+Python2 sqlmap.py
+
+;;
+
+8) apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/AdheBolo/AdminFinder
+echo "Menjalankan" | lolcat
+ls
+cd AdminFinder
+chmod 777 AdminFinder.py
+python2 AdminFinder.py
+
+;;
+
+9) git clone https://github.com/maldevel/IPGeolocation
+cd IPGeolocation
+chmod +x ipgeolocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -m
+python ipgeolocation.py -t " IP yang ingin dilacak "
+
+;;
+
+10) echo "Silahkan Chat Admin"
+xdg-open https://api.whatsapp.com/send?phone=6289619828046
+
+;;
+
+11) echo "silahkan Chat admin2"
+xdg-open https://api.whatsapp.com/send?phone=6283182378321
+
+;;
+
+12) apt update && apt upgrade
+pkg install git
+pkg install openssh
+pkg install bash
+git clone https://github.com/thelinuxchoice/saycheese
+ls
+cd saycheese
+ls
+bash saycheese.sh
+
+;;
+
+13) git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh install.sh
+sh gco.sh
+
+;;
+
+14) pkg install moon-buggy
+moon-buggy
+
+;;
+
+15) keluar
+clear
+toilet -f slant --gay "Thank You"
+toilet -f slant --gay "Kalau Suka Shere yaa"
+toilet -f slant --gay "$name shere yaa tools nya :)"
+exit
+esac
